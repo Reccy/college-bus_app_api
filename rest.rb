6ecbf38e9b12decff108431e602409c2557c4346
Aaron Meaney <aaronmeaney@hotmail.com>
2017-11-20 23:46:26 +0000

    Added basic bus position get and post.

--- rest.rb
+++ rest.rb
@@ -1,15 +1,33 @@
 require 'sinatra'
+require 'yaml'
 
-coords = ''
+latitude=''
+longitude=''
 
 # POST /bus_position
 # Updates the last posted bus position
 post '/api/bus_position' do
-    coords = request.body.read
+    bodyJSON = JSON.parse(unescape(request.body.read.to_s))
+    latitude = bodyJSON['latitude']
+    longitude = bodyJSON['longitude']
+    
+    status 200
+    body "{\"success\":\"true\",\"coords\":{\"latitude\":\"" + latitude + "\",\"longitude\":\"" + longitude + "\"}}"
 end
 
 # GET /bus_position
 # Returns the last posted bus position
 get '/api/bus_position' do
-    coords
+    if latitude == '' or longitude == '' then
+        status 202
+        body "{\"success\":\"false\",\"error_message\":\"Coordinates not yet POSTed.\"}"
+    else
+        status 200
+        body "{\"success\":\"true\",\"coords\":{\"latitude\":\"" + latitude + "\",\"longitude\":\"" + longitude + "\"}}"
+    end
+end
+
+# Unescapes a string
+def unescape(s)
+  YAML.load(%Q(---\n"#{s}"\n))
 end
