d4ade3c8f0b6e4ce85104b890ed6daddda01b888
Aaron Meaney <aaronmeaney@hotmail.com>
2017-11-30 15:59:24 +0000

    Added bus_route GET and POST methods.

--- rest.rb
+++ rest.rb
@@ -1,29 +1,73 @@
 require 'sinatra'
 require 'yaml'
 
-latitude=''
-longitude=''
+current_latitude = ''
+current_latitude = ''
+current_route = '';
 
 # POST /bus_position
 # Updates the last posted bus position
 post '/api/bus_position' do
-    bodyJSON = JSON.parse(unescape(request.body.read.to_s))
-    latitude = bodyJSON['latitude']
-    longitude = bodyJSON['longitude']
-    
-    status 200
-    body "{\"success\":\"true\",\"coords\":{\"latitude\":\"" + latitude + "\",\"longitude\":\"" + longitude + "\"}}"
+    begin
+        bodyJSON = JSON.parse(unescape(request.body.read.to_s))
+        current_latitude = bodyJSON['latitude']
+        current_latitude = bodyJSON['longitude']
+        
+        status 200
+        body "{\"success\":\"true\",\"bus_position\":{\"latitude\":\"" + current_latitude + "\",\"longitude\":\"" + current_latitude + "\"}}"
+    rescue StandardError => err
+        status 500
+        body "{\"success\":\"false\"}"
+    end
 end
 
 # GET /bus_position
 # Returns the last posted bus position
 get '/api/bus_position' do
-    if latitude == '' or longitude == '' then
-        status 202
-        body "{\"success\":\"false\",\"error_message\":\"Coordinates not yet POSTed.\"}"
-    else
+    begin
+        if current_latitude == '' or current_latitude == '' then
+            status 202
+            body "{\"success\":\"false\",\"error_message\":\"Coordinates not yet POSTed.\"}"
+        else
+            status 200
+            body "{\"success\":\"true\",\"bus_position\":{\"latitude\":\"" + current_latitude + "\",\"longitude\":\"" + current_latitude + "\"}}"
+        end
+    rescue StandardError => err
+        status 500
+        body "{\"success\":\"false\"}"
+    end
+end
+
+# POST /bus_route
+# Updates the bus's current route
+post '/api/bus_route' do
+    begin
+        bodyJSON = JSON.parse(unescape(request.body.read.to_s))
+        
+        current_route = bodyJSON;
+        
         status 200
-        body "{\"success\":\"true\",\"coords\":{\"latitude\":\"" + latitude + "\",\"longitude\":\"" + longitude + "\"}}"
+        body "{\"success\":\"true\",\"response\":" + current_route.to_json + "}"
+    rescue StandardError => err
+        status 500
+        body "{\"success\":\"false\",\"error_message\":\"" + err.to_s + "\"}"
+    end
+end
+
+# GET /bus_route
+# Returns the bus's current route
+get '/api/bus_route' do
+    begin
+        if current_route == '' then
+            status 202
+            body "{\"success\":\"false\",\"error_message\":\"Route not yet POSTed.\"}"
+        else
+            status 200
+            body "{\"success\":\"true\",\"response\":" + current_route.to_json + "}"
+        end
+    rescue StandardError => err
+        status 500
+        body "{\"success\":\"false\",\"error_message\":\"" + err.to_s + "\"}"
     end
 end
 
