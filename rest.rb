717bada406d0337e06976d6eec61bbfb0672ee04
Aaron Meaney <aaronmeaney@hotmail.com>
2018-05-10 15:48:02 +0000

    Added route_waypoints endpoint.

--- rest.rb
+++ rest.rb
@@ -5,7 +5,6 @@ require 'yaml'
 require 'pg'
 
 # Setup connection to the Postgres Database
-conn = nil
 begin
     conn = PG.connect(ENV['DATABASE_URL'])
     conn.exec "CREATE TABLE IF NOT EXISTS kvp (key varchar PRIMARY KEY, value varchar);"
@@ -14,29 +13,38 @@ end
 # Returns debug information
 get '/' do
     status 200
-    
     res = {'status' => 'ok'}
     return res.to_json
 end
 
 get '/bus_routes' do
-    status 200
+    res = nil
+    conn = PG.connect(ENV['DATABASE_URL'])
+    conn.transaction do |conn|
+        res = conn.exec "SELECT * FROM kvp WHERE key = 'bus_routes';"
+    end
+    conn.close
     
-    res = conn.exec "SELECT * FROM kvp WHERE key = 'bus_routes';"
+    return "empty" if res.nil?
+    
     res = Base64.decode64(res[0]['value'])
     
+    status 200
     return res
 end
 
 post '/bus_routes' do
-    status 200
     begin
         json = JSON.parse(unescape(request.body.read.to_s))
         
         writeval = Base64.encode64(json.to_json);
         
-        conn.exec "DELETE FROM kvp WHERE key = 'bus_routes';"
-        conn.exec "INSERT INTO kvp VALUES('bus_routes','#{writeval}');"
+        conn = PG.connect(ENV['DATABASE_URL'])
+        conn.transaction do |conn|
+            conn.exec "DELETE FROM kvp WHERE key = 'bus_routes';"
+            conn.exec "INSERT INTO kvp VALUES('bus_routes','#{writeval}');"
+        end
+        conn.close
         
         status 200
         return json.to_json
@@ -47,23 +55,33 @@ post '/bus_routes' do
 end
 
 get '/bus_stops' do
-    status 200
+    res = nil
+    conn = PG.connect(ENV['DATABASE_URL'])
+    conn.transaction do |conn|
+        res = conn.exec "SELECT * FROM kvp WHERE key = 'bus_stops';"
+    end
+    conn.close
     
-    res = conn.exec "SELECT * FROM kvp WHERE key = 'bus_stops';"
+    return "empty" if res.nil?
+    
     res = Base64.decode64(res[0]['value'])
     
+    status 200
     return res
 end
 
 post '/bus_stops' do
-    status 200
     begin
         json = JSON.parse(unescape(request.body.read.to_s))
         
         writeval = Base64.encode64(json.to_json);
         
-        conn.exec "DELETE FROM kvp WHERE key = 'bus_stops';"
-        conn.exec "INSERT INTO kvp VALUES('bus_stops','#{writeval}');"
+        conn = PG.connect(ENV['DATABASE_URL'])
+        conn.transaction do |conn|
+            conn.exec "DELETE FROM kvp WHERE key = 'bus_stops';"
+            conn.exec "INSERT INTO kvp VALUES('bus_stops','#{writeval}');"
+        end
+        conn.close
         
         status 200
         return json.to_json
@@ -71,4 +89,46 @@ post '/bus_stops' do
         status 500
         return "{\"success\":\"false\", \"error\":" + err.to_s + "}"
     end
+end
+
+get '/route_waypoints/:route' do
+    route_name = params[:route]
+    
+    key = "bus_waypoints_#{route_name}"
+    
+    res = nil
+    conn = PG.connect(ENV['DATABASE_URL'])
+    conn.transaction do |conn|
+        res = conn.exec "SELECT * FROM kvp WHERE key = '#{key}';"
+    end
+    conn.close
+    
+    return "empty" if res.nil?
+    
+    res = Base64.decode64(res[0]['value'])
+    
+    status 200
+    return res
+end
+
+post '/route_waypoints' do
+    begin
+        json = JSON.parse(unescape(request.body.read.to_s))
+        
+        internal_id = json['internal_id']
+        waypoints = json['waypoints']
+        writekey = "bus_waypoints_#{internal_id}"
+        writeval = Base64.encode64(waypoints.to_json)
+        
+        conn = PG.connect(ENV['DATABASE_URL'])
+        conn.transaction do |conn|
+            conn.exec "DELETE FROM kvp WHERE key = '#{writekey}';"
+            conn.exec "INSERT INTO kvp VALUES('#{writekey}','#{writeval}');"
+        end
+        conn.close
+        
+        status 200
+        return json.to_json
+    end
+    return 
 end
